e54eac1c87a0b87240bb6c561d8ce57f4209ad45
Vasili Pascal <jabberroid@gmail.com>
2013-10-25 17:29:09 +0300

    here is an explanation of how to clone existing reactions

--- example/repeat-reaction.py
+++ example/repeat-reaction.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from core.lib.twitter.search.sentense import Reaction
+from core.brain.hello import Reaction
+
+"""
+This is just a sample file you can use
+for creating a copy/alias of a reaction
+
+1.  Create a directory: mkdir core/brain/good/day/
+
+2.  Copy this file and rename it to reaction.py.
+    You will have something like: core/brain/good/morning/reaction.py
+
+3.  Now you will have the functionality of /core/brain/hello/reaction.py
+    in your new reaction
+
+4.  Now if you chat to your bot and say
+    "good morning" you will get a hello
+
+
+Another way to make it faster you need just tell your bot:
+    create new reaction  "good morning"
+
+"""
 
 
 class ReactionCopy(Reaction):
